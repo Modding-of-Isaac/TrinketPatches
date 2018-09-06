48622c332b2f0e9d2b4196dd8ded96d202fc6f88
martmists <martmists@gmail.com>
2018-09-06 16:46:36 +0200

    Stem Cell heals half health on new floor

--- trinkets/stem_cell.lua
+++ trinkets/stem_cell.lua
@@ -2,6 +2,9 @@ local f =
 TrinketPatches.util.register("MC_POST_NEW_LEVEL") ..
 function()
     local player = Isaac.GetPlayer(0)
+    if not player:HasFullHearts() then
+        player:AddHearts(player:GetMaxHearts()/2 - 1)  -- Heal half health
+    end
 
     if player:HasTrinket(TrinketType.TRINKET_STEM_CELL) then
         if (player:GetSoulHearts() > 0) then
